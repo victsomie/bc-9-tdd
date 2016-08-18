601b17a3c0f4e718cc965e53aa75279385e5834f
victsomie <victsomie@yahoo.com>
2016-08-18 23:46:34 +0300

    Adds supersumtest and supersum changes

--- supersum.py
+++ supersum.py
@@ -1,26 +1,36 @@
 def super_sum(*args):
+	'''Add the sum of given integers or items in a list(s) provided
+	'''
 
 	item_sum = 0
 
+    #Iterate the args
 	for item in args:
-		#Iterate the list if list found
+
+		#Check if it is a list and iterate through it
 		if type(item) == list:
-			for x in range(len(item)):
-			# for x in range(0, len(item)):
-			# 	if type(x) == int:
-			# 		item_sum += x
-			# 	elif type(x) == list:
-			# 		for j in x:
-			# 			item_sum += x[j]
-				item_sum += item[x]
+			for x in item:
+				#check if item is an integer there in and add it
+				if type(x) == int:
+					item_sum += x
+				#Check if item is a list inside another list and add items inside
+				elif type(x) == list:
+					for j in x:
+						item_sum += j
 
 		#Cummulate if integer
 		elif type(item) == int:
 			item_sum += item
+
 		else: 
 			return "Item is neither a number or list"
 	
 	return item_sum
 
 print super_sum(1, 2, 3, 4)
-print super_sum([2, 3, 4,5])
+print super_sum(100,200,300)
+
+print super_sum([2, 3, 4, 5, [5,6]], [4,5])
+print super_sum([5, 6], [4, 5], 10)
+
+print super_sum([20, 50], [5, 5], [10])
